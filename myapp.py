c9ee25e0f1e1774d72a4d321ef79cc11815e130b
Kishani Kandasamy <53715187+KishaniKandasamy@users.noreply.github.com>
2021-07-29 16:50:22 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -33,7 +33,7 @@ def initialize_app_for_new_client(q):
 
 def render_upload_view(q: Q):
     q.page['upload'] = ui.form_card(
-        box='1 2 3 -1',
+        box='1 2 3 6',
         items=[
             ui.separator(label='Choose a Dataset'),
             ui.message_bar(
@@ -54,7 +54,7 @@ def render_table_view(q: Q):
         items.append(ui.message_bar(type='success', text='Files uploaded successfully !'))
         items.append(make_ui_table(file_path=q.client.working_file_path, n_rows=100, name='head_of_table'))
 
-    q.page['table'] = ui.form_card(box='4 2 7 -1', items=items)
+    q.page['table'] = ui.form_card(box='4 2 7 6', items=items)
 
 def make_ui_table(file_path: str, n_rows: int, name: str):
     df = pd.read_csv(file_path)
@@ -85,7 +85,7 @@ async def handle_uploaded_data(q: Q):
     df_bar_stacked=  df.loc[:200,['AMT_INCOME_TOTAL','NAME_INCOME_TYPE','NAME_FAMILY_STATUS']]
     print(df_bar_stacked)
     v = q.page.add('df_bar_stacked', ui.plot_card(
-    box='1 7 10 5',
+    box='1 8 10 5',
     title='Make a stacked column plot',
     data=data(fields=df_bar_stacked.columns.tolist(),rows=df_bar_stacked.values.tolist()),
     plot=ui.plot(marks=[
@@ -98,7 +98,7 @@ async def handle_uploaded_data(q: Q):
     #make a point sized plot
     df_point_sized =  df.loc[:200,['AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY']]
     v = q.page.add('point_plot_sized', ui.plot_card(
-    box='1 12 5 2',
+    box='1 13 5 2',
     title='Make a scatterplot with mark sizes mapped to a continuous variable (a "bubble plot")',
     data=data(fields=df_point_sized.columns.tolist(),rows=df_point_sized.values.tolist()),
     plot=ui.plot([
@@ -110,7 +110,7 @@ async def handle_uploaded_data(q: Q):
     #make a point plot
     df_point =  df.loc[:200,['DAYS_REGISTRATION','DAYS_ID_PUBLISH','NAME_EDUCATION_TYPE']]
     v = q.page.add('point_plot', ui.plot_card(
-    box='6 12 5 2',
+    box='6 13 5 2',
     title='Make a scatterplot..',
     data=data(fields=df_point.columns.tolist(),rows=df_point.values.tolist()),
     plot=ui.plot([
@@ -123,7 +123,7 @@ async def handle_uploaded_data(q: Q):
     #make a line plot
     df_line=  df.loc[:200,['SK_ID_CURR','NAME_INCOME_TYPE','AMT_INCOME_TOTAL']]
     v = q.page.add('df_line', ui.plot_card(
-    box='1 14 5 2',
+    box='1 15 5 2',
     title='Make a line plot',
     data=data(fields=df_line.columns.tolist(),rows=df_line.values.tolist()),
     plot=ui.plot(marks=[
@@ -135,7 +135,7 @@ async def handle_uploaded_data(q: Q):
     #Make a line step plot
     df_line_step =  df.loc[:100,['SK_ID_CURR','NAME_INCOME_TYPE','AMT_INCOME_TOTAL']]
     v = q.page.add('df_heatmap', ui.plot_card(
-    box='6 14 5 2',
+    box='6 15 5 2',
     title='Make a line plot with a step curve',
     data=data(fields=df_line_step.columns.tolist(),rows=df_line_step.values.tolist()),
     plot=ui.plot([
@@ -147,7 +147,7 @@ async def handle_uploaded_data(q: Q):
     #Make an area plot
     df_area=  df.loc[:200,['AMT_INCOME_TOTAL','NAME_EDUCATION_TYPE','NAME_FAMILY_STATUS']]
     v = q.page.add('df_area', ui.plot_card(
-    box='1 16 10 4',
+    box='1 17 10 4',
     title='Make an area plot',
     data=data(fields=df_area.columns.tolist(),rows=df_area.values.tolist()),
     plot=ui.plot(marks=[
